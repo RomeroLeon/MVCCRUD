f2b0976ec45ec238e776cb07ac5f7331f2224b72
Migue <Migue@LEON>
2023-03-30 20:32:15 -0600

    Add project files.

---
+++ MVCCRUD/Models/Equipo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCCRUD.Models;
+
+public partial class Equipo
+{
+    public int IdMarcas { get; set; }
+    [Display(Name = "Nombre de la Marca")]
+    public string? NombreMarca { get; set; }
+    [Display(Name = "Estado")]
+
+    public string? Estados { get; set; }
+
+}
---
+++ MVCCRUD/Models/MvccrudContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class MvccrudContext : DbContext
+{
+    public MvccrudContext()
+    {
+    }
+
+    public MvccrudContext(DbContextOptions<MvccrudContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Equipo> Equipos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            //warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+            //        => optionsBuilder.UseSqlServer("Server=localhost; Database=MVCCRUD; Trusted_Connection=True; TrustServerCertificate=True;");
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Equipo>(entity =>
+        {
+            entity.HasKey(e => e.IdMarcas).HasName("PK__Equipos__EB594FAD142F2369");
+
+            entity.Property(e => e.IdMarcas).HasColumnName("Id_marcas");
+            entity.Property(e => e.Estados)
+                .HasMaxLength(1)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.NombreMarca)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("Nombre_marca");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
